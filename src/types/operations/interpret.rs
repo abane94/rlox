960b550607f106c6af0150a139da0a041c7164aa
Aris Husanu <abane94@gmail.com>
2024-05-25 20:20:43 -0400

    Inital implementation of interprit

--- src/types/operations/interpret.rs
+++ src/types/operations/interpret.rs
@@ -1,7 +1,12 @@
 use crate::token_type::Token;
 use crate::types::expr::Expr;
-use crate::types::Literal;
 use crate::types::stmt::Stmt;
+use crate::types::Literal;
+
+use super::Interpret;
+
+#[derive(Debug)]
+pub struct RuntimeError(pub String);
 
 pub fn assign(name: &Token, value: &Box<Expr>) -> Literal {
     todo!()
@@ -12,15 +17,24 @@ pub fn binary(left: &Box<Expr>, operator: &Token, right: &Box<Expr>) -> Literal 
 }
 
 pub fn grouping(expression: &Box<Expr>) -> Literal {
-    todo!()
+    return expression.interpret();
 }
 
 pub fn literal(value: &Literal) -> Literal {
     todo!()
 }
 
-pub fn unary(operator: &Token, right: &Box<Expr>) -> Literal {
-    todo!()
+pub fn unary(operator: &Token, _right: &Box<Expr>) -> Literal {
+    let right = _right.interpret();
+    match operator.token_type {
+        token_type::TokenType::MINUS => match right {
+            Literal::String(_) => RuntimeError("Invalid Cast: Cannot negate String"),
+            Literal::Number(num) => -num,
+            Literal::Boolean(_) => RuntimeError("Invalid Cast: Cannot negate Boolean, use '!'"),
+            Literal::None() => RuntimeError("Invalid Cast: Cannot negate Empty value"),
+        },
+        _ => !panic("INvalid unary token"),
+    }
 }
 
 pub fn variable(name: &Token) -> Literal {
